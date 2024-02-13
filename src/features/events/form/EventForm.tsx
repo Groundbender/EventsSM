0a13d6d9e30d69e70cbe196700174605bb1ea49e
Damir <damir24may@mail.ru>
2024-02-14 00:28:41 +0300

    react-hook-form and custom semantic ui select integration and validation

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,81 +1,59 @@
-import { ChangeEvent, useState } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { Button, Form, Header, Segment } from "semantic-ui-react"
 import { useAppDispatch, useAppSelector } from "../../../app/store/store";
 import { createEvent, updateEvent } from "../eventSlice";
 import { createId } from "@paralleldrive/cuid2";
+import { Controller, FieldValues, useForm } from "react-hook-form";
+import { categoryOptions } from "./categoryOptions";
 
 
 const EventForm = () => {
+  const { register, handleSubmit, control, setValue, formState: { errors, isValid, isSubmitting } } = useForm({
+    mode: 'onBlur',
+
+  });
   let { id } = useParams();
   const event = useAppSelector(state => state.events.events.find(event => event.id === id))
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
-  const initialValues = event ?? {
-    title: '',
-    category: '',
-    description: '',
-    city: '',
-    venue: '',
-    date: ''
-  }
 
 
-  const [values, setValues] = useState(initialValues)
 
 
-  function onSubmit() {
-    console.log(values);
-    id = id ?? createId()
-
-    event ? dispatch(updateEvent({ ...event, ...values })) :
-      dispatch(createEvent({
-        ...values,
-        hostedBy: "Bob",
-        attendees: [],
-        hostPhotoURL: "/assets/user.png",
-        id
-      }))
+  function onSubmit(data: FieldValues) {
 
 
-    navigate(`/events/${id}`)
+    console.log(data);
+
 
   }
 
-  function handleInputsChange(e: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-
-
-    setValues({
-      ...values,
-      [name]: value
-    })
-  }
 
   return (
     <Segment clearing>
-      <Header content={event ? 'Edit the event' : 'Create new event'} />
-      <Form onSubmit={onSubmit}>
-        <Form.Field>
-          <input type="text" placeholder="Event title" name="title" value={values.title} onChange={e => handleInputsChange(e)} />
-        </Form.Field>
-        <Form.Field>
-          <input name="category" value={values.category} onChange={e => handleInputsChange(e)} type="text" placeholder="Category" />
-        </Form.Field>
-        <Form.Field>
-          <input name="description" value={values.description} onChange={e => handleInputsChange(e)} type="text" placeholder="Description" />
-        </Form.Field>
-        <Form.Field>
-          <input name="city" value={values.city} onChange={e => handleInputsChange(e)} type="text" placeholder="City" />
-        </Form.Field>
-        <Form.Field>
-          <input name="venue" value={values.venue} onChange={e => handleInputsChange(e)} type="text" placeholder="Venue" />
-        </Form.Field>
-        <Form.Field>
-          <input name="date" value={values.date} onChange={e => handleInputsChange(e)} type="date" placeholder="Date" />
-        </Form.Field>
+      <Header content="Event Details" sub color="teal" />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Input placeholder="Event title" defaultValue={event?.title || ""} {...register("title", { required: true })} error={errors.title && "Title is required"} />
 
-        <Button type="submit" floated="right" positive content="Submit" />
+
+        <Controller name="category" control={control} rules={{ required: "Category is required" }} defaultValue={event?.category} render={({ field }) => (
+          <Form.Select clearable options={categoryOptions} placeholder=" Category" {...field} onChange={(_, data) => setValue("category", data.value)} error={errors.category && errors.category.message} />
+
+
+        )} />
+
+
+
+        <Form.TextArea placeholder="Description" defaultValue={event?.description || ""} {...register("description", { required: "Description is required" })} error={errors.description && errors.description.message} />
+
+        <Header sub content="Location details" color="teal" />
+
+        <Form.Input placeholder="City" defaultValue={event?.city || ""} {...register("city", { required: "City is required" })} error={errors.city && errors.city.message} />
+
+        <Form.Input placeholder="Venue" defaultValue={event?.venue || ""} {...register("venue", { required: "Venue is required" })} error={errors.venue && errors.venue.message} />
+
+        <Form.Input type="date" placeholder="Date" defaultValue={event?.date || ""} {...register("date", { required: "Date is required" })} error={errors.date && errors.date.message} />
+        <Button disabled={!isValid || isSubmitting} type="submit" floated="right" positive content="Submit" />
         <Button as={Link} to="/events" type="button" floated="right" content="Cancel" />
       </Form>
     </Segment>
