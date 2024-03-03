6cda131b8c4048e181677ec12a44ce6935dccf53
Damir <damir24may@mail.ru>
2024-03-03 16:34:34 +0300

    follow and unfollow functionality firebase, and subcollection follwers and following

--- src/app/types/profile.ts
+++ src/app/types/profile.ts
@@ -4,10 +4,18 @@ export type Profile = {
   photoUrl: string
   createdAt: string
   description: string
+  followerCount?: number
+  followingCount?: number
 }
 
 export type Photo = {
   id: string
   name: string
   url: string
+}
+
+export type Follow = {
+  id: string
+  displayName: string
+  photoURL: string
 }
--- src/features/profiles/ProfileHeader.tsx
+++ src/features/profiles/ProfileHeader.tsx
@@ -1,11 +1,61 @@
 import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from "semantic-ui-react"
 import { Profile } from "../../app/types/profile"
+import { useFireStore } from "../../app/hooks/firestore/useFirestore"
+import { auth } from "../../app/config/firebase"
+import { increment } from "firebase/firestore"
 
 
 type Props = {
   profile: Profile
 }
 const ProfileHeader = ({ profile }: Props) => {
+
+  const { update } = useFireStore("profiles");
+  // берем профиль из коллекции по id профиля чтобы увеличить ему followerCount
+  // set для обновления подколлекции
+  const { set: setFollower, remove: removeFollower } = useFireStore(`profiles/${profile.id}/followers`)
+
+
+  // а здесь берем id автоирзованного usera и увеличиваем его followingCount ( нас увеличивается кол-во подиписок)
+  // set для обновления подколлекции
+  const { set: setFollowing, remove: removeFollowing } = useFireStore(`profiles/${auth.currentUser?.uid}/following`)
+
+
+
+  async function handleFollowToggle(follow: boolean) {
+    if (!profile.id || !auth.currentUser?.uid) {
+      return
+    }
+    if (follow) {
+      await update(auth.currentUser.uid, {
+        followingCount: increment(1)
+      })
+      await update(profile.id, {
+        followerCount: increment(1)
+      })
+
+      await setFollowing(profile.id, {
+        displayName: profile.displayName,
+        photoURL: profile.photoUrl
+      })
+      await setFollower(auth.currentUser.uid, {
+        displayName: auth.currentUser.displayName,
+        photoURL: auth.currentUser.photoURL
+      })
+    } else {
+      await update(auth.currentUser.uid, {
+        followingCount: increment(-1)
+      })
+      await update(profile.id, {
+        followerCount: increment(-1)
+      })
+
+      await removeFollowing(profile.id)
+      await removeFollower(auth.currentUser.uid)
+    }
+  }
+
+
   return (
     <Segment>
       <Grid>
@@ -21,18 +71,20 @@ const ProfileHeader = ({ profile }: Props) => {
         </Grid.Column>
         <Grid.Column width={4}>
           <Statistic.Group>
-            <Statistic label="Followers" value={10} />
-            <Statistic label="Following" value={5} />
+            <Statistic label="Followers" value={profile.followerCount || 0} />
+            <Statistic label="Following" value={profile.followingCount || 0} />
           </Statistic.Group>
           <Divider />
-          <Reveal animated="move">
+          <Button onClick={() => handleFollowToggle(true)} color="teal" content="Follow" />
+          <Button onClick={() => handleFollowToggle(false)} color="red" content="Unfollow" />
+          {/* <Reveal animated="move">
             <Reveal.Content visible style={{ width: "100%" }}>
               <Button fluid color="teal" content="Following" />
             </Reveal.Content>
             <Reveal.Content hidden style={{ width: "100%" }}>
               <Button basic fluid color="red" content="Unfollow" />
             </Reveal.Content>
-          </Reveal>
+          </Reveal> */}
         </Grid.Column>
       </Grid>
     </Segment>
